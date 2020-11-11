cd2391658d12dc40f444e5eb3ffc4d0f9033b2cb
GandresCoello18 <goyeselcoca@gmail.com>
2020-11-11 16:19:42 -0500

    remplace domain in producion api

--- src/config/domain.ts
+++ src/config/domain.ts
@@ -1,6 +1,6 @@
 import Cookies from "js-cookie";
 
 export const DEVELOPMENT: string = "http://127.0.0.1:7000";
-export const PRODUCTION: string = "https://api-seguro-social.herokuapp.com";
-export const DOMAIN: string = DEVELOPMENT;
+export const PRODUCTION: string = "https://vacunas-5-esquinas.herokuapp.com";
+export const DOMAIN: string = PRODUCTION;
 export const AUTH_USER: string | undefined = Cookies.get("id-user");
